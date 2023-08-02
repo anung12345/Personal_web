24cf1ccf9257133f3ec6ed4a03e39e1f8ae3ad90
anung12345 <hanindito.hr@gmail.com>
2023-08-03 01:20:22 +0300

    Create option form

--- pages/Contact_Us.py
+++ pages/Contact_Us.py
@@ -1,15 +1,20 @@
 import streamlit as st
 from send_email import send_email
+import pandas
+
+df = pandas.read_csv("topics.csv")
 
 st.header("Contact Me")
 
 with st.form(key="email_form"):
     user_email = st.text_input("Your email address")
+    select = st.selectbox("What topic do you want to discuss", df["topic"])
     raw_message = st.text_area("Your message")
     message = f"""\
 Subject: New email from {user_email}
 
 from: {user_email}
+Topic: {select}
 {raw_message}
 """
     button = st.form_submit_button("Submit")
