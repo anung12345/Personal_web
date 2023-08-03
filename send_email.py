8bb3db493f5de00458cfe7d9f2ddc41aa5c081c0
anung12345 <hanindito.hr@gmail.com>
2023-08-03 18:24:39 +0300

    Encrypted password

--- send_email.py
+++ send_email.py
@@ -1,16 +1,16 @@
 import smtplib
 import ssl
+import os
 
 
 def send_email(message):
-        host = "smtp.gmail.com"
-        port = 465
-        username = "hanindito.hr@gmail.com"
-        password = "amryknpzyyeyxogb"
-        receiver = "anunghirmawan170704@gmail.com"
-        context = ssl.create_default_context()
+    host = "smtp.gmail.com"
+    port = 465
+    username = "hanindito.hr@gmail.com"
+    password = os.getenv("PASSWORD")
+    receiver = "anunghirmawan170704@gmail.com"
+    context = ssl.create_default_context()
 
-        with smtplib.SMTP_SSL(host, port, context=context) as server:
-            server.login(username, password)
-            server.sendmail(username, receiver, message)
-
+    with smtplib.SMTP_SSL(host, port, context=context) as server:
+        server.login(username, password)
+        server.sendmail(username, receiver, message)
